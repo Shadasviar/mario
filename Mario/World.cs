903b153d38cf6113c5f9867bd0b99e1540b65c16
Shadasviar <shadasviarus@gmail.com>
2017-03-27 01:17:47 -0700

    Plug world implemented

--- Mario/World.cs
+++ Mario/World.cs
@@ -8,5 +8,37 @@ namespace Mario
 {
     class World
     {
+        private SortedList<Coordinates, Unit> units;
+        public enum Side { Top, Left, up, Down}; //Temp enum
+
+        public void addUnit(Unit unit)
+        {
+
+        }
+
+        public Unit getUnit(Coordinates coord)
+        {
+            return new Unit();
+        }
+
+        public bool move(Unit unit, Coordinates dst)
+        {
+            return true;
+        }
+
+        public bool move(Coordinates src, Coordinates dst)
+        {
+            return true;
+        }
+
+        public bool remove(Unit unit)
+        {
+            return true;
+        }
+
+        public bool interaction(Unit unit1, Side side1, Unit unit2, Side side2)
+        {
+            return true;
+        }
     }
 }
