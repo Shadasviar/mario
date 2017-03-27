6753ac9868aff572a91c0f18a1f909f3ca292bdd
Shadasviar <shadasviarus@gmail.com>
2017-03-27 00:12:33 -0700

    Empty classes added

---
+++ Mario/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mario
+{
+    class Game : GameAPI
+    {
+        public bool addWorld(World world)
+        {
+            throw new NotImplementedException();
+        }
+
+        public World getWorld(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool move(Coordinates src, Coordinates dst)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool playerIsLaive()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Mario/GameAPI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mario
+{
+    interface GameAPI
+    {
+        bool move(Coordinates src, Coordinates dst);
+        bool playerIsLaive();
+        World getWorld(int index);
+        bool addWorld(World world);
+
+    }
+}
