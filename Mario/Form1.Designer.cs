82c1331d75d2411765373e2824b01b82b6dddeb3
Elenoes <maselenno@gmail.com>
2017-03-30 12:03:58 +0200

    Visual

--- Mario/Form1.Designer.cs
+++ Mario/Form1.Designer.cs
@@ -29,11 +29,72 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.field = new System.Windows.Forms.Panel();
+            this.player2 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.field.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // field
+            // 
+            this.field.Controls.Add(this.player2);
+            this.field.Controls.Add(this.player);
+            this.field.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.field.Location = new System.Drawing.Point(0, 0);
+            this.field.Name = "field";
+            this.field.Size = new System.Drawing.Size(790, 295);
+            this.field.TabIndex = 1;
+            // 
+            // player2
+            // 
+            this.player2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.player2.Location = new System.Drawing.Point(566, 0);
+            this.player2.Name = "player2";
+            this.player2.Size = new System.Drawing.Size(98, 72);
+            this.player2.TabIndex = 3;
+            this.player2.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.player.Location = new System.Drawing.Point(350, 213);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(132, 79);
+            this.player.TabIndex = 2;
+            this.player.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 295);
+            this.Controls.Add(this.field);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.field.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel field;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox player2;
     }
 }
 
--- Mario/Form1.cs
+++ Mario/Form1.cs
@@ -12,9 +12,93 @@ namespace Mario
 {
     public partial class Form1 : Form
     {
+        bool right;
+        bool left;
+
+        bool rp2;
+        bool lp2;
+
+
+        bool jump;
+        int G = 30;
+        int force;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(right == true) { player.Left += 5; }
+            if (left == true) { player.Left -= 5; }
+            if(jump == true )
+            {
+                player.Top -= force;
+                force -= 1;
+            }
+            if(player.Top + player.Height >= field.Height)
+            {
+                player.Top = field.Height - player.Height;
+                jump = false;
+            }
+            else
+            {
+                player.Top += 5;
+
+            }
+
+
+
+            if (rp2 == true) { player2.Left += 5; }
+            if (lp2 == true) { player2.Left -= 5; }
+            if (jump == true)
+            {
+                player2.Top -= force;
+                force -= 1;
+            }
+            if (player2.Top + player2.Height >= field.Height)
+            {
+                player2.Top = field.Height - player2.Height;
+                jump = false;
+            }
+            else
+            {
+                player2.Top += 5;
+
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Right) { right = true; }
+            if(e.KeyCode == Keys.Left) { left = true; }
+
+            if (e.KeyCode == Keys.D) { rp2 = true; }
+            if (e.KeyCode == Keys.A) { lp2 = true; }
+
+            if (e.KeyCode == Keys.Escape) { this.Close(); } //Escape => Exit
+
+            if (jump != true)
+            {
+                if (e.KeyCode == Keys.Space)
+                    jump = true;
+                force = 30;
+                        }
+            if (jump != true)
+            {
+                if (e.KeyCode == Keys.W)
+                    jump = true;
+                force = 30;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Right) { right = false; }
+            if(e.KeyCode == Keys.Left) { left = false; }
+            if (e.KeyCode == Keys.D) { rp2 = false; }
+            if (e.KeyCode == Keys.A) { lp2 = false; }
         }
     }
 }
