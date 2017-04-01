494c7a44d8354ff76c96f62f9d2dc51dc72ad313
KovalchM <kmmmmk770@gmail.com>
2017-04-01 17:40:12 +0200

    Coordinates -

--- Mario/Coordinates.cs
+++ Mario/Coordinates.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace Mario
 {
     class Coordinates
     {
+
+        public Point bottomLeft = new Point  ();
+        public Point topRight = new Point ();
+
+
     }
 }
