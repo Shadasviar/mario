40fb29dce01a361d73f34293f6c5be5ba35daf24
KovalchM <kmmmmk770@gmail.com>
2017-04-03 19:58:11 +0200

    Constructors for points and 4 variables

--- Mario/Coordinates.cs
+++ Mario/Coordinates.cs
@@ -9,11 +9,28 @@ namespace Mario
 {
     class Coordinates
     {
+        int a, b, c, d;
+        int cord_x, cord_y;
 
-        public Point bottomLeft = new Point  ();
+        public Point bottomLeft = new Point();
 
-        public Point topRight = new Point ();
+        public Point topRight = new Point();
 
+        public Coordinates(int cord_x, int cord_y)
+            {
+            this.cord_x = cord_x;
+            this.cord_y = cord_y;
+
+        }
+
+        public Coordinates(int a, int b, int c, int d)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+
+        }
 
     }
 }
