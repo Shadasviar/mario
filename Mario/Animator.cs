a145fce4cc8d9bb579ba8a7caa67b5070f06f138
Anastas19 <buniianastasiia1998@gmail.com>
2017-05-03 21:22:42 +0200

    Animator class

---
+++ Mario/Animator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace GameEngine
+{
+    class Animator
+    {
+        List<Image> fragmens;
+        public void reset ()
+        {
+            return;
+        }
+        public Image getNextFrame()
+        {
+            return new Image();
+        }
+        public Animator (List<Image> frames, int game_fps = 0)
+        {
+
+        } 
+
+    }
+}
