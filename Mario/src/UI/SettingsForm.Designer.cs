661a6df09aa979b418a342f194384f4b317e850e
kovalchm <kmmmmk770@gmail.com>
2017-06-17 23:22:32 +0200

    FPS

--- Mario/src/UI/SettingsForm.Designer.cs
+++ Mario/src/UI/SettingsForm.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.n_players_comboBox = new System.Windows.Forms.ComboBox();
+            this.FPS_changing = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -38,9 +40,10 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(7, 8);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(121, 17);
+            this.label1.Size = new System.Drawing.Size(93, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Players number";
             // 
@@ -50,20 +53,45 @@
             this.n_players_comboBox.Items.AddRange(new object[] {
             "1",
             "2"});
-            this.n_players_comboBox.Location = new System.Drawing.Point(139, 6);
+            this.n_players_comboBox.Location = new System.Drawing.Point(104, 5);
+            this.n_players_comboBox.Margin = new System.Windows.Forms.Padding(2);
             this.n_players_comboBox.Name = "n_players_comboBox";
-            this.n_players_comboBox.Size = new System.Drawing.Size(121, 24);
+            this.n_players_comboBox.Size = new System.Drawing.Size(92, 21);
             this.n_players_comboBox.TabIndex = 1;
             this.n_players_comboBox.SelectedIndexChanged += new System.EventHandler(this.n_players_comboBox_SelectedIndexChanged);
             // 
+            // FPS_changing
+            // 
+            this.FPS_changing.Location = new System.Drawing.Point(104, 31);
+            this.FPS_changing.Name = "FPS_changing";
+            this.FPS_changing.Size = new System.Drawing.Size(91, 20);
+            this.FPS_changing.TabIndex = 2;
+            this.FPS_changing.TextChanged += new System.EventHandler(this.FPS_changing_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(7, 38);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Changing FPS\r\n";
+            // 
             // SettingsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Mario.Properties.Resources.background;
-            this.ClientSize = new System.Drawing.Size(383, 317);
+            this.ClientSize = new System.Drawing.Size(287, 258);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.FPS_changing);
             this.Controls.Add(this.n_players_comboBox);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "SettingsForm";
             this.Text = "Settings";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.SettingsForm_FormClosed);
@@ -76,5 +104,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox n_players_comboBox;
+        private System.Windows.Forms.TextBox FPS_changing;
+        private System.Windows.Forms.Label label2;
     }
 }
--- Mario/src/UI/SettingsForm.cs
+++ Mario/src/UI/SettingsForm.cs
@@ -22,5 +22,11 @@ namespace Global
         {
             parent.Show();
         }
+
+        private void FPS_changing_TextChanged(object sender, EventArgs e)
+        {
+            // if((Settings.Default.fps != null) && (Settings.Default.fps != < 0)) something gets wrong или не нужно было проверять так как в Parse уже занесены 3 исключения ?
+            Settings.Default.fps = Int32.Parse(this.FPS_changing.SelectedText.ToString());
+        }
     }
 }
